829193b3f289d159b8059000ad6b267678edae37
ashayp22 <ashayparikh22@gmail.com>
2020-09-18 22:36:34 -0500

    all good

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from 'react-bootstrap/Button';
+import GameScreen from './components/GameScreen.js';
+import InstructionScreen from './components/InstructionScreen.js';
+import MenuScreen from './components/MenuScreen.js';
+import ResearchScreen from './components/ResearchScreen.js';
+import RestartScreen from './components/RestartScreen.js';
+import VisualScreen from './components/VisualScreen.js';
 
 
-function App() {
-  return (
-    <div>
-      <p>Hello world</p>
-      <Button variant="primary">Primary</Button>
-    </div>
-  );
+export default class App extends React.Component{
+  render() {
+      return (
+          <div className="gameScreen">
+              <div className = "title">
+                  Stock App
+              </div>
+              <div className = "buttons">
+                  <Button onClick = {() => this.handleClick()} variant="primary">Priry</Button>{' '}
+                  <Button onClick = {() => this.handleClick()} variant="secondary">Secondary</Button>{' '}
+              </div>
+              <GameScreen></GameScreen> 
+              <InstructionScreen></InstructionScreen> 
+              <MenuScreen></MenuScreen> 
+              <ResearchScreen></ResearchScreen> 
+              <RestartScreen></RestartScreen> 
+              <VisualScreen></VisualScreen> 
+          </div>
+      );
+  }
 }
 
-export default App;
