cc3afa4a8e27d62e77f83fb30945b2d8de78052b
ashayp22 <ashayparikh22@gmail.com>
2020-09-19 13:28:25 -0500

    yugs code now works

--- backend/twitter.py
+++ backend/twitter.py
@@ -1,13 +1,13 @@
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('hSoczapCa0itwTX7tSI0F2Mnu3bYwX93B10QInnn0gC_')
+authenticator = IAMAuthenticator('0TzdiYxO3LQfYzOJyLy2pBThmH6x8f9IAYHs3-p3g_sP')
 tone_analyzer = ToneAnalyzerV3(
     version='2017-09-21',
     authenticator=authenticator
 )
 
-tone_analyzer.set_service_url('https://api.us-east.tone-analyzer.watson.cloud.ibm.com')
+tone_analyzer.set_service_url('https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/dd65f5dc-0904-4dfa-b66e-2e7c67fb5c1a')
 
 import requests
 from requests_html import HTMLSession
@@ -24,17 +24,16 @@ def get_tweets(q):
     try:
         session = HTMLSession()
         today = datetime.today().strftime("%Y-%m-%d")
-        five_years_before = (datetime.today() - timedelta(days=365*5)).strftime("%Y-%m-%d")
+        five_years_before = (datetime.today() - timedelta(days=5)).strftime("%Y-%m-%d")
         response = session.get("https://twitter.com/search?q=" + q + "%20since%3A"+ five_years_before +"%20until%3A" + today +"&src=typd")  # gets the page
-        response.html.render(sleep=2)  # gets a page full of javascript, runs the javascript, waits 2 seconds
+        response.html.render(sleep=5)  # gets a page full of javascript, runs the javascript, waits 2 seconds
         a = response.html.find(
             'span')  # now we have an html page, find the span (which contains the tweets and other text)
         for f in a:
             t = f.text
-            if len(
-                    t) > 20:  # you can have either a blacklist and remove text that aren't tweets. I chose to keep all text over 20 charaters in length
-                print(f.text)
-                tweets.append(t)
+            if len(t) > 20:  # you can have either a blacklist and remove text that aren't tweets. I chose to keep all text over 20 charaters in length
+                if t != "Don’t miss what’s happening" and t != 'People on Twitter are the first to know.':
+                    tweets.append(t)
 
     except requests.exceptions.RequestException as e:
         print(e)
@@ -50,17 +49,34 @@ def add_dict(dict_one, key):
 def analyze_tweets(company_name):
     tones = {}
     tweets = get_tweets(company_name + " stock")
+
+    text = ""
+
     for tweet in tweets:
-        tone_analysis = tone_analyzer.tone(
-            {'text': tweet},
-            content_type='application/json'
-        ).get_result()
-        for sentence in tone_analysis["sentence_tone"]:
-            for tone in sentence["tones"]:
-                if tone["tone_id"] in tones.keys():
-                    tones[tone["tone_id"]] += tone["score"]
-                else:
-                    tones[tone["tone_id"]] = tone["score"]
-    factor = 1.0 / sum(tones.itervalues())
-    normalised_tones = {k: v * factor for k, v in tones.iteritems()}
-    return normalised_tones
+        text += tweet + "."
+
+    print(text)
+
+    tone_analysis = tone_analyzer.tone(
+        {'text': text},
+        content_type='application/json'
+    ).get_result()
+
+    print(tone_analysis)
+
+    for sentence in tone_analysis["sentences_tone"]:
+        for tone in sentence["tones"]:
+            if tone["tone_id"] in tones.keys():
+                tones[tone["tone_id"]] += tone["score"]
+            else:
+                tones[tone["tone_id"]] = tone["score"]
+    s = 0
+    for a in tones:
+        s += tones[a]
+
+    for a in tones:
+        tones[a] /= s
+
+    return tones
+
+analyze_tweets("apple")
