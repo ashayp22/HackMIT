09953a73666d35318eb3587022f8c8fe0693d914
ashayp22 <ashayparikh22@gmail.com>
2020-09-19 22:53:14 -0500

    all good now

--- frontend/src/components/RestartScreen.js
+++ frontend/src/components/RestartScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from 'react-bootstrap/Button';
-
+import './style/extra.css'
 
 export default class RestartScreen extends React.Component{
 
@@ -45,9 +45,9 @@ export default class RestartScreen extends React.Component{
               <h1 className = "header">Game Over</h1>
                 <p>Final Score: {this.props.finalScore}</p>
                 <p>{this.props.message}</p>
-              <button className = "coolButton .b2" onClick = {() => this.props.click(1)}>Play Again</button>
+              <button className = "coolButton b2" onClick = {() => this.props.click(1)}>Play Again</button>
               <br></br>
-              <button className = "coolButton .back" onClick = {() => this.props.click(0)}>Go to Home</button>
+              <button className = "coolButton back" onClick = {() => this.props.click(0)}>Go to Home</button>
             </div>
         </div>
       );
